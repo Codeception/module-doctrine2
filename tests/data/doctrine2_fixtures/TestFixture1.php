cc5b4e4912bcfc2f9fb901a7105da3ea7c841d94
anboo <developer@rusdteam.ru>
2020-09-24 19:02:50 +0300

    Fix old version doctrine tests

--- tests/data/doctrine2_fixtures/TestFixture1.php
+++ tests/data/doctrine2_fixtures/TestFixture1.php
@@ -1,15 +1,27 @@
 <?php
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
-use Doctrine\Persistence\ObjectManager;
 
-class TestFixture1 implements FixtureInterface
-{
-    public function load(ObjectManager $manager)
+if (class_exists('Doctrine\Persistence\ObjectManager')) {
+    class TestFixture1 implements FixtureInterface
     {
-        $entity = new PlainEntity();
-        $entity->setName('from TestFixture1');
-        $manager->persist($entity);
-        $manager->flush();
+        public function load(Doctrine\Persistence\ObjectManager $manager)
+        {
+            $entity = new PlainEntity();
+            $entity->setName('from TestFixture1');
+            $manager->persist($entity);
+            $manager->flush();
+        }
+    }
+} elseif (class_exists('Doctrine\Common\Persistence\ObjectManager')) {
+    class TestFixture1 implements FixtureInterface
+    {
+        public function load(Doctrine\Common\Persistence\ObjectManager $manager)
+        {
+            $entity = new PlainEntity();
+            $entity->setName('from TestFixture1');
+            $manager->persist($entity);
+            $manager->flush();
+        }
     }
 }
--- tests/data/doctrine2_fixtures/TestFixture2.php
+++ tests/data/doctrine2_fixtures/TestFixture2.php
@@ -1,15 +1,28 @@
 <?php
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
-use Doctrine\Persistence\ObjectManager;
 
-class TestFixture2 implements FixtureInterface
-{
-    public function load(ObjectManager $manager)
+if (class_exists('Doctrine\Persistence\ObjectManager')) {
+    class TestFixture2 implements FixtureInterface
     {
-        $entity = new PlainEntity();
-        $entity->setName('from TestFixture2');
-        $manager->persist($entity);
-        $manager->flush();
+        public function load(Doctrine\Persistence\ObjectManager $manager)
+        {
+            $entity = new PlainEntity();
+            $entity->setName('from TestFixture2');
+            $manager->persist($entity);
+            $manager->flush();
+        }
+    }
+} elseif (class_exists('Doctrine\Common\Persistence\ObjectManager')) {
+    class TestFixture2 implements FixtureInterface
+    {
+        public function load(Doctrine\Common\Persistence\ObjectManager $manager)
+        {
+            $entity = new PlainEntity();
+            $entity->setName('from TestFixture2');
+            $manager->persist($entity);
+            $manager->flush();
+        }
     }
 }
+
