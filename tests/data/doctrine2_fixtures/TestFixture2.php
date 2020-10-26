1ae32bdcd77abde4a583462f5949fc86b9bda2a2
Daniel Andreyev <anboo@users.noreply.github.com>
2020-10-26 09:32:52 +0300

    Configurable purge mode - DELETE or TRUNCATE

--- tests/data/doctrine2_fixtures/TestFixture2.php
+++ tests/data/doctrine2_fixtures/TestFixture2.php
@@ -1,15 +1,28 @@
 <?php
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
-use Doctrine\Common\Persistence\ObjectManager;
 
-class TestFixture2 implements FixtureInterface
-{
-    public function load(ObjectManager $manager)
+if (version_compare(PHP_VERSION, '7.1', '>')) {
+    class TestFixture2 implements FixtureInterface
     {
-        $entity = new PlainEntity();
-        $entity->setName('from TestFixture2');
-        $manager->persist($entity);
-        $manager->flush();
+        public function load(Doctrine\Persistence\ObjectManager $manager)
+        {
+            $entity = new PlainEntity();
+            $entity->setName('from TestFixture2');
+            $manager->persist($entity);
+            $manager->flush();
+        }
+    }
+} else {
+    class TestFixture2 implements FixtureInterface
+    {
+        public function load(\Doctrine\Common\Persistence\ObjectManager $manager)
+        {
+            $entity = new PlainEntity();
+            $entity->setName('from TestFixture2');
+            $manager->persist($entity);
+            $manager->flush();
+        }
     }
 }
+
